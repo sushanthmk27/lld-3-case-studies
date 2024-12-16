b1ecd0e3cafeaf7d00075b0925a9a007aa526ff5
sushanthmk27 <mksushanth0@gmail.com>
2024-12-16 19:30:28 +0530

    Changes

--- src/tictactoe/Client.java
+++ src/tictactoe/Client.java
@@ -1,4 +1,28 @@
 package tictactoe;
 
+import tictactoe.controllers.GameController;
+import tictactoe.models.Game;
+import tictactoe.models.enums.GameState;
+
 public class Client {
+    public static void main(String[] args){
+        GameController gameController = new GameController();
+
+
+
+        gameController.startGame();
+
+        while(gameController.gameState().equals(GameState.IN_PROGRESS)){
+            gameController.display();
+            gameController.makeMove();
+        }
+
+        if(gameController.gameState().equals(GameState.SUCCESS)){
+            System.out.println("Winner of the game - " +gameController.getWinner().getName());
+        }
+        else if(gameController.gameState().equals(GameState.DRAW)){
+            System.out.println("Game is drawn");
+        }
+    }
+
 }
--- src/tictactoe/controllers/GameController.java
+++ src/tictactoe/controllers/GameController.java
@@ -1,4 +1,31 @@
 package tictactoe.controllers;
 
+import tictactoe.models.Player;
+import tictactoe.models.enums.GameState;
+
 public class GameController {
+
+    public void startGame(){
+
+    }
+
+    public GameState gameState(){
+        return null;
+    }
+
+    public void display(){
+
+    }
+
+    public void makeMove(){
+
+    }
+
+    public void undo(){
+
+    }
+
+    public Player getWinner(){
+        return null;
+    }
 }
